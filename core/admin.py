beef83e17a173e3c5b0cd7cd080fac67c030fdd8
ewanchukwilliam <ewanchukwilliam@gmail.com>
2025-01-02 04:56:43 -0700

    i think I added in a new pythonic view for the applciation?

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from core.models import Blog, Comment
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('blog', 'author', 'created_at', 'gold_star')
+    list_filter = ('created_at', 'gold_star')
+    search_fields = ('content',)
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
 # Register your models here.
